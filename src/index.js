003a97a0171bb44e60416f0035215d8d1e69eba9
keisuke <rememberthatk@icloud.com>
2020-05-06 20:39:19 +0900

    [master] add react-router-dom

--- src/index.js
+++ src/index.js
@@ -5,12 +5,47 @@ import { CssBaseline } from '@material-ui/core'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 
 import App from './App'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import theme from './theme'
+import { VideoProvider } from './components/VideoProvider'
 import * as serviceWorker from './serviceWorker'
+
+const connectionOptions = {
+  bandwidthProfile: {
+    video: {
+      mode: 'collaboration',
+      dominantSpeakerPriority: 'standard',
+      renderDimensions: {
+        high: { height: 1080, width: 1920 },
+        standard: { height: 720, width: 1280 },
+        low: { height: 90, width: 160 },
+      },
+    },
+  },
+  dominantSpeaker: true,
+  maxAudioBitrate: 12000,
+  networkQuality: { local: 1, remote: 1 },
+  preferredVideoCodecs: [{ codec: 'VP8', simulcast: true }],
+}
+
+const VideoApp = () => {
+  return (
+    <VideoProvider options={connectionOptions}>
+      <App />
+    </VideoProvider>
+  )
+}
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
+    <Router>
+      <Switch>
+        <Route exact path='/' component={VideoApp}/>
+        <Route path='/room/:URLRoomName' component={VideoApp}/>
+        <Redirect to='/'/>
+      </Switch>
+    </Router>
     <App />
   </MuiThemeProvider>,
   document.getElementById('root')
